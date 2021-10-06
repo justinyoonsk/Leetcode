7f621439e650688efc811375adfcefcff43c2bd1
yoons7 <yoons7@mcmaster.ca>
2021-10-06 13:43:43 -0400

    adjusted leetcode solution

--- 0046_permutations.py
+++ 0046_permutations.py
@@ -4,15 +4,15 @@ class Solution:
         if len(nums) == 1:
             return [nums[:]]
         
-        for i in range(len(nums)): #1,2,3.   #3, 2
-            n = nums.pop(0) #1.  #2   #3
-            perms = self.permute(nums) #2,3    #3    #2
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
             
             for perm in perms:
-                perm.append(n) #3, 2
-            result.extend(perms) #3, 2, 
+                perm.append(n) 
+            result.extend(perms) 
             print(result)
-            nums.append(n) #2
+            nums.append(n)
             
         return result
     
